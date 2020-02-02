87cd836bfc38ffae63b483259cb1d2e42f0c8f76
Matthew Fernandez <mattfer@gmail.com>
2020-02-02 02:06:36 -0800

    works now

--- app/app.py
+++ app/app.py
@@ -1,10 +1,12 @@
 from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
 from flask_migrate import Migrate
+from flask_cors import CORS
 from app.config import Config
 
 
 app = Flask(__name__)
+CORS(app)
 app.config.from_object(Config)
 
 db = SQLAlchemy(app)
--- app/controller/car_controller.py
+++ app/controller/car_controller.py
@@ -1,10 +1,12 @@
 from flask import Blueprint, request, Response, jsonify
 from app.models import car
-from app.controller.carSelector import rateCars, user_choices_test, cars_test
+from app.controller.carSelector import rateCars, user_choices_test, cars_test, database
 
 car_controller = Blueprint('car_controller', __name__)
+MAX_CARS_TO_SEND = 6
 
-@car_controller.route('/')
+
+@car_controller.route('/', methods=['GET', 'POST'])
 def getCars():
     if request.is_json:
         userData = request.get_json()
@@ -13,15 +15,14 @@ def getCars():
 
     results = car.Car.query.all()
     serialized = [c.serialize() for c in results]
-    rated_cars = rateCars(cars_test, user_choices_test)
+    rated_cars = rateCars(database, userData)
 
     sort_func = lambda e: e["points"]
     rated_cars.sort(key=sort_func, reverse=True)
 
     result_cars = []
-    print(len(rated_cars))
     result_i = 0
-    while (result_i < 5 and  result_i < len(rated_cars)):
+    while (result_i < MAX_CARS_TO_SEND and  result_i < len(rated_cars)):
         result_cars.append(rated_cars[result_i])
         result_i += 1
 
